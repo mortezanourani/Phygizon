2f3e94b04dc8f607ef62e5b8b73a40f1e29869c8
Morteza Nourani <mortezanourani@gmail.com>
2024-05-13 18:53:32 +0330

    Update signup page
    - Phone number input updated to use country code list.

--- src/website/SignupPage.js
+++ src/website/SignupPage.js
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 
 import Layout from "./Layout";
+import { PhoneInput } from "react-international-phone";
+import 'react-international-phone/style.css';
 
 const SignupPage = () => {
     const navigate = useNavigate();
@@ -11,37 +13,30 @@ const SignupPage = () => {
         window.location.replace('/dashboard/');
     }
 
-    const [formData, setFormData] = useState({
-        email: '',
-        firstname: '',
-        lastname: '',
-        country: '',
-        phonenumber: '',
-    });
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    }
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        await axios.post('https://phygizone.darkube.app/v1/user/register/user/', {
-            email: formData.email,
-            first_name: formData.firstname,
-            last_name: formData.lastname,
-            country: formData.country,
-            phone_number: formData.phonenumber,
-        })
-            .then(response => {
-                if (response.status === 201) {
-                    localStorage.setItem('register', 'token ' + response.data.token_info.token);
-                    navigate('/signup/verify/', { replace: true });
-                }
-            }).catch(error => {
-                alert(error.response.data.message);
-            });
+        const formData = {
+            email: email,
+            first_name: firstname,
+            last_name: lastname,
+            country: document.querySelector('.phone button').getAttribute('data-country'),
+            phone_number: phone,
+        };
+        await axios.post('https://phygizone.darkube.app/v1/user/register/user/', formData
+        ).then(response => {
+            if (response.status === 201) {
+                localStorage.setItem('register', 'token ' + response.data.token_info.token);
+                navigate('/signup/verify/', { replace: true });
+            }
+        }).catch(error => {
+            alert(error.response.data.message);
+        });
     }
 
     return (
@@ -53,13 +48,17 @@ const SignupPage = () => {
                         <h4>Welcome to Phygizon</h4>
                         <p>We are glad to see you here.</p>
                     </span>
-                    <input type="email" className="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} />
-                    <input className="firstname" name="firstname" placeholder="First Name" value={formData.firstname} onChange={handleChange} />
-                    <input className="lastname" name="lastname" placeholder="Last Name" value={formData.lastname} onChange={handleChange} />
-                    <div className="phone">
-                        <input className="country" name="country" placeholder="Country" value={formData.country} onChange={handleChange} />
-                        <input type="tel" className="phonenumber" name="phonenumber" placeholder="Phone Number" value={formData.phonenumber} onChange={handleChange} />
-                    </div>
+                    <input type="email" className="email" name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input className="firstname" name="firstname" placeholder="First Name" value={firstname} onChange={(e) => setFirstname(e.target.value)} />
+                    <input className="lastname" name="lastname" placeholder="Last Name" value={lastname} onChange={(e) => setLastname(e.target.value)} />
+                    <PhoneInput
+                        className="phone"
+                        inputClassName="phonenumber"
+                        defaultCountry="ca"
+                        name="phonenumber"
+                        value={phone}
+                        onChange={(phone) => setPhone(phone)}
+                    />
 
                     <button type="submit" className="btn cta lg">Continue</button>
                 </form>
